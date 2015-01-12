58b93f8c65ab6dad564b3b87aaa7b39076e04e03
pjwl33 <paul.leejw@gmail.com>
2015-01-12 17:14:20 -0500

    anchor scrollgs

--- js/app/controllers/HomeCtrl.js
+++ js/app/controllers/HomeCtrl.js
@@ -1,9 +1,13 @@
-ang.controller('HomeCtrl', function($scope, $timeout, $interval, parallaxHelper) {
+ang.controller('HomeCtrl', function($scope, $timeout, $interval, parallaxHelper, $location, $anchorScroll) {
 
     $scope.background = parallaxHelper.createAnimator(-0.7, 100, -100);
 
     $scope.setTab = function(tab){
-      $scope.activeTab = tab;
+      // $scope.activeTab = tab;
+      $location.hash(tab);
+      $anchorScroll();
     };
+
+
 
 });
